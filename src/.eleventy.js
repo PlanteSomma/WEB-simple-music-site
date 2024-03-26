50e941d7bd28e1e9ded2b2b55c67f6e23ca4c53d
Alexander Somma <admin@openco.ca>
2024-03-26 12:57:52 -0100

    Add passthrough copy for robots.txt

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function(eleventyConfig) {
     // Copy `src/files` to `_site/files`
     eleventyConfig.addPassthroughCopy({"files": "files"});
-  
+    // Put robots.txt in root
+eleventyConfig.addPassthroughCopy({ 'robots.txt': 'robots.txt' }); 
     return {
       // other configuration...
     };
