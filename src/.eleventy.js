803fd97b644789de981bb17c8395ef7cd01ebb96
OpenCo <admin@openco.ca>
2024-08-01 10:16:03 +0000

    chore: adding 404 passthough copy

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -2,7 +2,8 @@ module.exports = function(eleventyConfig) {
     // Copy `src/files` to `_site/files`
     eleventyConfig.addPassthroughCopy({"files": "files"});
     // Put robots.txt in root
-eleventyConfig.addPassthroughCopy({ 'robots.txt': 'robots.txt' }); 
+eleventyConfig.addPassthroughCopy({ 'robots.txt': 'robots.txt' });
+eleventyConfig.addPassthroughCopy({ '404.html':'404.html'}) 
     return {
       // other configuration...
     };
